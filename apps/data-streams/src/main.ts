ee3de4870bf2b0fc2d7ab198db672ca6d5f9152b
Marko Mitranic <marko.mitranic@gmail.com>
2022-03-10 20:02:32 +0000

    Parametrize application ports

--- apps/data-streams/src/main.ts
+++ apps/data-streams/src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useLogger(LoggerFactory.createLogger(AppModule.name));
-  await app.listen(3000);
+  await app.listen(process.env.APP_PORT);
 }
 
 bootstrap();
--- apps/worker/src/main.ts
+++ apps/worker/src/main.ts
@@ -1,13 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { WorkerModule } from './worker.module';
-import { MicroserviceOptions } from '@nestjs/microservices';
+import { LoggerFactory } from 'apps/util/util.logger.factory';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(WorkerModule, {
-    // Setup communication protocol here
-  });
-  app.listen(async () => {
-    console.log('Microservice is listening');
-  });
+  const app = await NestFactory.create(WorkerModule);
+  app.useLogger(LoggerFactory.createLogger(WorkerModule.name));
+  await app.listen(process.env.WORKER_PORT);
 }
 bootstrap();
