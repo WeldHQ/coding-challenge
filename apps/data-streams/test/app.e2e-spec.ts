4a414a5e969390180441762079f626971c929e91
Marko Mitranic <marko.mitranic@gmail.com>
2022-03-12 17:43:42 +0000

    Cover results API with e2e tests

--- apps/data-streams/test/app.e2e-spec.ts
+++ apps/data-streams/test/app.e2e-spec.ts
@@ -2,23 +2,106 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { ClientProxy, ClientsModule, Transport } from '@nestjs/microservices';
+import { Observable } from 'rxjs';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let client: ClientProxy;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [
+        AppModule,
+        ClientsModule.register([
+          { name: 'DATA_STREAMS_MS', transport: Transport.TCP },
+        ]),
+      ],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.connectMicroservice({ transport: Transport.TCP });
+
+    await app.startAllMicroservicesAsync();
     await app.init();
+
+    client = app.get('DATA_STREAMS_MS');
+    await client.connect();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterEach((done) => {
+    client.close();
+    app.close();
+    done();
+  });
+
+  it('[GET] /private/results/TEST returns data', async (done) => {
+    const message = {
+      adapter: 'TEST',
+      timestamp: 12345678,
+      payload: {
+        id: 'id1',
+        filename: 'filename.json',
+        rawData: { message: 1 },
+      },
+    };
+    const emitter: Observable<any> = client.emit('results', message);
+    await emitter.subscribe();
+
+    const response = await request(app.getHttpServer())
+      .get('/private/results/TEST')
+      .send();
+
+    expect(response.status).toBe(200);
+    expect(response.body).toMatchObject({ data: [{ message: 1 }] });
+    done();
+  });
+
+  it('[GET] /private/results/TEST returns multiple pieces of data in order', async (done) => {
+    const message = {
+      adapter: 'TEST',
+      timestamp: 12345678,
+      payload: {
+        id: 'id1',
+        filename: 'filename.json',
+        rawData: { message: 1 },
+      },
+    };
+    const emitter: Observable<any> = client.emit('results', message);
+    await emitter.subscribe();
+
+    const message2 = {
+      adapter: 'TEST',
+      timestamp: 12345678,
+      payload: {
+        id: 'id1',
+        filename: 'filename.json',
+        rawData: { message: 2 },
+      },
+    };
+    const emitter2: Observable<any> = client.emit('results', message2);
+    await emitter2.subscribe();
+
+    const response = await request(app.getHttpServer())
+      .get('/private/results/TEST')
+      .send();
+
+    expect(response.status).toBe(200);
+    expect(response.body).toMatchObject({
+      data: [{ message: 1 }, { message: 2 }],
+    });
+    done();
+  });
+
+  it('[GET] /private/results/TEST returns a 404 response when no data', async (done) => {
+    const response = await request(app.getHttpServer())
+      .get('/private/results/TEST')
+      .send();
+
+    expect(response.status).toBe(404);
+    expect(response.body.message).toEqual(
+      'The datastore does not contain stream: TEST',
+    );
+    done();
   });
 });
